f3e45badc8a3d6ab29d0ce7c4388d84f253ce6ee
adarshrwl <adarshrawal321@gmail.com>
2023-01-19 14:46:12 +0545

    Small changes

--- Model/loginadmin1.java
+++ Model/loginadmin1.java
@@ -1,33 +1,34 @@
 package Model;
 
-public class loginadmin1{
+public class loginadmin1 {
     // instance variable
     private String Username;
     private String Password;
-    
+    // Getting Username
+
     public String getUsername() {
         return this.Username;
     }
+    // setting username
 
     public void setUsername(String Username) {
         this.Username = Username;
     }
+    // getting password
 
     public String getPassword() {
         return this.Password;
     }
+    // setting password
 
     public void setPassword(String Password) {
         this.Password = Password;
     }
 
-    
-    
-
     // public constructor of Stucent Class
-    loginadmin1(String Username,String Password){
+    loginadmin1(String Username, String Password) {
         this.Username = Username;
         this.Password = Password;
     }
-    
+
 }
