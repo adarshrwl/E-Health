c2618cd69f8f970a08daa07828764b7e7b5568d1
adarshrwl <adarshrawal321@gmail.com>
2023-02-03 22:57:19 +0545

    Added more controller files

--- Controller/dashcontroller.java
+++ Controller/dashcontroller.java
@@ -24,22 +24,3 @@ public class dashcontroller {
     }
 
 }
-
-    public int insertStudent(Dashboard dashboard) {
-
-        int id = dashboard.getStd_id();
-        String name = dashboard.getStd_name();
-        String address = dashboard.getStd_address();
-        int age = dashboard.getStd_age();
-        int contact = dashboard.getStd_contacr();
-        String insertQuery = String.format(
-                "insert into tbl_student(std_id,std_name,std_age,std_address,std_contacr) values(%d,'%s',%d,'%s',%d)",
-                id,
-                name, age, address, contact);
-        System.out.println(insertQuery);
-        dbConnection = new DbConnection();
-        int result = dbConnection.manipulate(insertQuery);
-        return result;
-    }
-
-}
