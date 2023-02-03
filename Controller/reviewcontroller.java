c5f32aa969bcc03865b29d727bf2f65e57c67a49
adarshrwl <adarshrawal321@gmail.com>
2023-02-03 22:44:49 +0545

    Codes

--- Controller/reviewcontroller.java
+++ Controller/reviewcontroller.java
@@ -3,28 +3,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
  */
 package controller;
-a
+
 import Database.DbConnection;
 import model.reviewmodel;
 
 public class reviewcontroller {
   DbConnection dbConnection;
-  public int  insert_review(reviewmodel model){
-      String doctorname=model.getDoctorname();
-      String specilization=model.getSpecilization();
-      String comment=model.getComment();
-      int id=1;
-       String insertQuery = String.format(
-      "INSERT INTO comment_table VALUES(%d,'%s', '%s', '%s')",id,doctorname,specilization,comment 
-      );
-         
-  dbConnection = new DbConnection();
+
+  public int insert_review(reviewmodel model) {
+    String doctorname = model.getDoctorname();
+    String specilization = model.getSpecilization();
+    String comment = model.getComment();
+    int id = 1;
+    String insertQuery = String.format(
+        "INSERT INTO comment_table VALUES(%d,'%s', '%s', '%s')", id, doctorname, specilization, comment);
+
+    dbConnection = new DbConnection();
 
     int result = dbConnection.manipulate(insertQuery);
     return result;
   }
-   
-    public static void main(String args[]) {
-        // TODO code application logic here
-    }
+
+  public static void main(String args[]) {
+    // TODO code application logic here
+  }
 }
